4403c865e0ca48d009cad3c9c5c079a8de9b5423
Josh Cantie <jcantie3@gmail.com>
2026-06-29 23:44:28 -0400

    #15 added a restart button (no-op currently)

--- message_banner.py
+++ message_banner.py
@@ -1,3 +1,4 @@
+from direct.gui.DirectGui import DirectButton
 from direct.gui.OnscreenText import OnscreenText
 from direct.showbase.ShowBase import ShowBase
 from panda3d.core import TextNode
@@ -12,6 +13,12 @@ STROKES_SUBTITLE_SCALE = 0.08
 
 ELAPSED_SUBTITLE_POSITION = (0, 0.45)
 ELAPSED_SUBTITLE_SCALE = 0.08
+
+RESTART_BUTTON_POSITION = (0, 0, 0.325)
+RESTART_BUTTON_SCALE = 0.08
+RESTART_BUTTON_PAD = (0.3, 0.3)
+RESTART_BUTTON_TEXT_SCALE = 0.7
+RESTART_LABEL = "Restart"
 
 WIN_MESSAGE = "You did it!"
 
@@ -28,6 +35,14 @@ class MessageBanner:
         self.elapsed_subtitle: OnscreenText = self._make_line(
             ELAPSED_SUBTITLE_POSITION, ELAPSED_SUBTITLE_SCALE
         )
+        self.restart_button: DirectButton = DirectButton(
+            text=RESTART_LABEL,
+            scale=RESTART_BUTTON_SCALE,
+            pos=RESTART_BUTTON_POSITION,
+            pad=RESTART_BUTTON_PAD,
+            text_scale=RESTART_BUTTON_TEXT_SCALE,
+        )
+        self.restart_button.hide()
 
     def _make_line(self, position: tuple[float, float], scale: float) -> OnscreenText:
         """Build one hidden, centered line of banner text."""
@@ -48,6 +63,7 @@ class MessageBanner:
         self.show(WIN_MESSAGE, f"Strokes: {stroke_count}")
         self.elapsed_subtitle.setText(self.format_elapsed(elapsed_seconds))
         self.elapsed_subtitle.show()
+        self.restart_button.show()
 
     def format_elapsed(self, elapsed_seconds: float) -> str:
         """Player-facing elapsed time, in seconds and milliseconds."""
@@ -65,3 +81,4 @@ class MessageBanner:
         self.text.hide()
         self.strokes_subtitle.hide()
         self.elapsed_subtitle.hide()
+        self.restart_button.hide()
